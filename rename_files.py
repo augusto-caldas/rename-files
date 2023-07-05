2091955b48709bfc6c9601638cd1cfa297d67f0b
Luis Augusto Caldas <42305127+augusto-caldas@users.noreply.github.com>
2023-07-05 16:20:31 +0100

    Add files via upload

---
+++ rename_files.py
@@ -0,0 +1,17 @@
+import os
+import re
+
+def rename_files(directory):
+    for filename in os.listdir(directory):
+        # Remove content within [] and ()
+        new_filename = re.sub(r'\[.*?\]|\(.*?\)', '', filename)  
+        # Remove whitespace
+        new_filename = new_filename.strip()  
+        # Only rename if there are changes
+        if new_filename != filename:  
+            os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+            # Print feedback to user
+            print(f"Renamed '{filename}' to '{new_filename}'")
+
+directory_path = './'
+rename_files(directory_path)
