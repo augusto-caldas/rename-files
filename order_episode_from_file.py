235da6b19a2352a01c378649f10c1825627eee0f
Luis Caldas <42305127+augusto-caldas@users.noreply.github.com>
2024-07-12 00:08:24 +0100

    Fix script to support more video types

--- order_episode_from_file.py
+++ order_episode_from_file.py
@@ -18,7 +18,7 @@ with open(text_file_path, 'r') as file:
 # Walk through all directories and files
 for dirpath, dirnames, filenames in os.walk(root_dir):
     for filename in filenames:
-        if filename.endswith(".avi" | ".mkv" | ".mp4" | ".mov"):
+        if filename.endswith(".avi", ".mkv", ".mp4", ".mov"):
             episode_name = filename[:-4]
             if episode_name in episode_dict:
                 new_filename = f"{episode_dict[episode_name]} - {filename}"
